0c336b49ddf8f38133fa5a38de716a501a787ca1
Adil Khan <khan.0adil@gmail.com>
2017-02-24 13:23:04 -0800

    Firsty

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,7 @@
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BankAccount {
 
   private double balance;
@@ -14,5 +18,19 @@ public class BankAccount {
     balance -= amount;
     return amount;
   }
+
+}
+
+
+public class TestBankAccount {
+  
+  @Test
+  public void testDebitwithSuff()
+  {
+    BankAccount acct = new BankAccount(10);
+    double amt = acct.debit(5);
+    Assert.assertEquals(5.0,amt); 
+  }
+
 
 }
