3414d4e3c87db76f505266f7be0e76cb7a073fce
Adil Khan <khan.0adil@gmail.com>
2017-02-24 13:14:00 -0800

    First Test Case

---
+++ src/BankAccount Test.java
@@ -0,0 +1,16 @@
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBankAccount {
+
+  @Test
+  public void testDebitwithSuff()
+  {
+    BankAccount acct = new BankAccount(10);
+    double amt = account.debit(5);
+    Assert.assertEquals(5.0,amount); 
+  }
+
+}
